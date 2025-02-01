c85492badf64e576eae188684d85456b97a0fecd
bellagonzalez27 <bellagonzalez1127@gmail.com>
2025-02-01 12:07:54 -0500

    updating PlayerMovement to left and right arrow keys

--- slopeLab/Assets/Scripts/PlayerMovement.cs
+++ slopeLab/Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,10 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A)){
+        if(Input.GetKey(KeyCode.LeftArrow)){
             rb.AddForce(Vector3.left * speed);
         }
-        if(Input.GetKey(KeyCode.D)){
+        if(Input.GetKey(KeyCode.RightArrow)){
             rb.AddForce(Vector3.right * speed);
         }
     }
