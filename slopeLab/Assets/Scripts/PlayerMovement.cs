6e4348d0381fea5d0571a44d969331081771fd33
midnight-ocean <micheleproy@gmail.com>
2025-02-01 20:17:56 -0500

    part 3!!

--- slopeLab/Assets/Scripts/PlayerMovement.cs
+++ slopeLab/Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,19 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
     [SerializeField] float speed;
+    [SerializeField] float startSpeed;
+    [SerializeField] float timeStartSpeed; 
+    float timeStartSpeedCounter;
     // Update is called once per frame
+    void Start() {
+    }
     void Update()
-    {
+    {   
+        if (timeStartSpeedCounter <= timeStartSpeed) {
+        rb.AddForce(Vector3.forward * startSpeed); 
+        timeStartSpeedCounter+=1; 
+        }
+
         if(Input.GetKey(KeyCode.LeftArrow)){
             rb.AddForce(Vector3.left * speed);
         }
