3e28b9e69ba2523931d03c16531456b05f83a827
midnight-ocean <micheleproy@gmail.com>
2025-02-04 20:51:30 -0500

    fixed bugs

--- slopeLab/Assets/Scripts/NewPlatform.cs
+++ slopeLab/Assets/Scripts/NewPlatform.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 
 public class NewPlatform : MonoBehaviour
 {
+    public int platformCount = 0;
+    public bool alreadyCollided = false;
+
     [SerializeField] GameObject[] platforms;
     void OnTriggerEnter(Collider other){
-        if(other.gameObject.tag == "Player"){
-            Instantiate(platforms[Random.Range(0, platforms.Length)], 
-            new Vector3(transform.position.x, transform.position.y-2, transform.position.z+8), Quaternion.identity);
+        if(!alreadyCollided){
+            alreadyCollided = true;
+            
+            Debug.Log("Platform Count: " + platformCount);
+            platformCount++;
+
+            if(other.gameObject.tag == "Player"){
+                Instantiate(platforms[Random.Range(0, platforms.Length)], 
+                new Vector3(transform.position.x, transform.position.y-2, transform.position.z+8), Quaternion.identity);
+            }
         }
     }
 }
