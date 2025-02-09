22afc63d7df5ccb93642aa9cd838d6727d6eb294
bellagonzalez27 <bellagonzalez1127@gmail.com>
2025-02-09 04:09:01 -0500

    some minor changes attempting to make the road spawn infinitely (it's better but not infinite)

--- slopeLab/Assets/Scripts/NewPlatform.cs
+++ slopeLab/Assets/Scripts/NewPlatform.cs
@@ -3,20 +3,36 @@ using UnityEngine;
 public class NewPlatform : MonoBehaviour
 {
     public int platformCount = 0;
-    public bool alreadyCollided = false;
-
+    [SerializeField] int maxPlatforms = 100; // Set a larger limit
     [SerializeField] GameObject[] platforms;
-    void OnTriggerEnter(Collider other){
-        if(!alreadyCollided){
-            alreadyCollided = true;
-            
-            Debug.Log("Platform Count: " + platformCount);
-            platformCount++;
 
-            if(other.gameObject.tag == "Player"){
-                Instantiate(platforms[Random.Range(0, platforms.Length)], 
-                new Vector3(transform.position.x, transform.position.y-2, transform.position.z+8), Quaternion.identity);
-            }
+    void Start()
+    {
+        Destroy(gameObject, 15f); // Destroy old platforms after 15 seconds
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && platformCount < maxPlatforms)
+        {
+            Debug.Log("Triggered by: " + other.gameObject.name);
+
+            Instantiate(
+                platforms[Random.Range(0, platforms.Length)],
+                new Vector3(transform.position.x, transform.position.y - 2, transform.position.z + 8),
+                Quaternion.identity
+            );
+
+            platformCount++;
+        }
+    }
+
+    void Update()
+    {
+        // Reset platform count if no platforms exist
+        if (GameObject.FindGameObjectsWithTag("Platform").Length == 0)
+        {
+            platformCount = 0;
         }
     }
 }
